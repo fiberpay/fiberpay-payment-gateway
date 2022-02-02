0d8bb908c58c95ab1ccd1afc198cac4ff95d0459
Jedrzej Jarocki <jedrzej.jarocki@inpay.pl>
2022-02-02 15:11:24 +0100

    add uninstall file

---
+++ uninstall.php
@@ -0,0 +1,21 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// if uninstall not called from WordPress exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+/*
+ * Only remove ALL product and page data if WC_REMOVE_ALL_DATA constant is set to true in user's
+ * wp-config.php. This is to prevent data loss when deleting the plugin from the backend
+ * and to ensure only the site owner can perform this action.
+ */
+// if ( defined( 'WC_REMOVE_ALL_DATA' ) && true === WC_REMOVE_ALL_DATA ) {
+	// Delete options.
+	delete_option( 'woocommerce_stripe_settings' );
+	delete_option( 'wc_fiberpay_show_keys_notice' );
+	delete_option( 'woocommerce_fiberpay_payments_settings' );
+// }
